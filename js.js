1492124a81eda1cf736cb058a7dea79526e9bcdf
Simon Lee <simonlee98853@gmail.com>
2022-12-24 19:11:22 -0500

    fix calculating algorithm based on specifications of Odin project

--- js.js
+++ js.js
@@ -1,17 +1,74 @@
 //variables
-let displayStr = "";
-
+let num = ""; //operands
+let op = ""; //operators
+let past = ""; //control variable for what operator does
+console.log(op);
 //dom elements
 const result = document.querySelector(".result");
-const buttons = document.querySelectorAll(".button-container > .button");
+const pastResult = document.querySelector(".past-result");
+const numbers = document.querySelectorAll("[data-number]");
+const operators = document.querySelectorAll("[data-operator]");
 const clear = document.querySelector(".button");
+const equal = document.querySelector("[data-equal]");
 
 clear.addEventListener("click", clearResult);
-buttons.forEach((button) => button.addEventListener("click", getInput));
-
+numbers.forEach((button) => button.addEventListener("click", makeNumber));
+operators.forEach((button) => button.addEventListener("click", setOperator));
+equal.onclick = () => equalFn();
 function clearResult() {
+  past = '';
+  pastResult.textContent = '';
   result.textContent = "";
+  num='';
+  op = "";
 }
+function equalFn() {
+  if(/^[0-9]+[+\-*/][0-9]+$/.test(past)){
+    numArray = past.split(/[\+\-\*\/]/); // splits based on operators
+    let calculation = applyOp(op, parseInt(numArray[0]), parseInt(numArray[1]));
+    pastResult.textContent = numArray[0] + op + numArray[1] + "=";
+    result.textContent = calculation;
+    num='';
+    op = "";
+    past = calculation;
+  }
+  else
+    return;//not correct past form
+}
+
+function makeNumber(e) {
+  num += e.target.dataset.number;
+  past += e.target.dataset.number;
+  result.textContent = past;
+  console.log(past);
+}
+
+function setOperator(e) {
+  //when there is an op and past is of form 'number op'
+  if (op != "" && /^[0-9]+[+\-*/]$/.test(past)) {
+    return;
+  }
+  //when there is an op and past is of form 'num op num'
+  else if (op != "" && /^[0-9]+[+\-*/][0-9]+$/.test(past)) {
+    numArray = past.split(/[\+\-\*\/]/); // splits based on operators
+    let calculation = applyOp(op, parseInt(numArray[0]), parseInt(numArray[1]));
+    console.log(past);
+    pastResult.textContent = numArray[0] + op + numArray[1] + "=";
+    op = e.target.dataset.operator;
+    result.textContent = calculation+op;
+    past = calculation + op;
+    num ='';
+  } else if ((op === '')) {
+    op = e.target.dataset.operator;
+    result.textContent = num+op;
+    past += op;
+    num = "";
+  }
+  else
+    return;// some edge case like putting operator first
+}
+
+
 function getInput(e) {
   if (Object.keys(e.target.dataset)[0] === "number") {
     //opPressed ? (number += button.dataset.number): (number2 +=button.dataset.number);
@@ -23,88 +80,8 @@ function getInput(e) {
       // total =operate(op, total, num);
       // opPressed = false;
     } else {
-        result.textContent += e.target.dataset.operator;
+      result.textContent += e.target.dataset.operator;
     }
-  }
-}
-
-function evaluate(expression) {
-  let tokens = expression.split("");
-  let values = [];
-  let ops = [];
-  for (let i = 0; i < tokens.length; i++) {
-    // Current token is a number,
-    // push it to stack for numbers
-    if (tokens[i] >= "0" && tokens[i] <= "9") {
-      let sbuf = "";
-      // There may be more than
-      // one digits in number
-      while (i < tokens.length && tokens[i] >= "0" && tokens[i] <= "9") {
-        sbuf = sbuf + tokens[i++];
-      }
-      values.push(parseInt(sbuf, 10));
-      // Right now the i points to
-      // the character next to the digit,
-      // since the for loop also increases
-      // the i, we would skip one
-      //  token position; we need to
-      // decrease the value of i by 1 to
-      // correct the offset.
-      i--;
-    }
-    // Current token is an opening
-    // brace, push it to 'ops'
-    else if (tokens[i] == "(") {
-      ops.push(tokens[i]);
-    }
-    // Closing brace encountered,
-    // solve entire brace
-    else if (tokens[i] == ")") {
-      while (ops[ops.length - 1] != "(") {
-        values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-      }
-      ops.pop();
-    }
-    // Current token is an operator.
-    else if (
-      tokens[i] == "+" ||
-      tokens[i] == "-" ||
-      tokens[i] == "*" ||
-      tokens[i] == "/"
-    ) {
-      // While top of 'ops' has same
-      // or greater precedence to current
-      // token, which is an operator.
-      // Apply operator on top of 'ops'
-      // to top two elements in values stack
-      while (ops.length > 0 && hasPrecedence(tokens[i], ops[ops.length - 1])) {
-        values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-      }
-      // Push current token to 'ops'.
-      ops.push(tokens[i]);
-    }
-  }
-  // Entire expression has been
-  // parsed at this point, apply remaining
-  // ops to remaining values
-  while (ops.length > 0) {
-    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
-  }
-  // Top of 'values' contains
-  // result, return it
-  return values.pop();
-}
-// Returns true if 'op2' has
-// higher or same precedence as 'op1',
-// otherwise returns false.
-function hasPrecedence(op1, op2) {
-  if (op2 == "(" || op2 == ")") {
-    return false;
-  }
-  if ((op1 == "*" || op1 == "/") && (op2 == "+" || op2 == "-")) {
-    return false;
-  } else {
-    return true;
   }
 }
 // A utility method to apply an
@@ -122,7 +99,9 @@ function applyOp(op, a, b) {
       return power(a, b);
     case "/":
       if (b == 0) {
-        document.write("Cannot divide by zero");
+        document.write(
+          "Cannot divide by zero. Because it will be infinity or something"
+        );
       }
       return divide(a, b);
     default:
@@ -145,7 +124,7 @@ const power = function (a, b) {
   return Math.pow(a, b);
 };
 const divide = function (a, b) {
-  return a/b;
+  return a / b;
 };
 const factorial = function (a) {
   if (a == 0) return 1;
